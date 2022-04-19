6a9a71b991cba36eedda2c6c10adcf4cfc0f077c
chizi56 <bauka1215@gmail.com>
2022-04-19 22:18:35 +0600

    init: initializae project

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import Home from './components/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Favorites from './components/Favorites';
+import { useState } from 'react/cjs/react.production.min';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const Tab = createMaterialBottomTabNavigator();
+const [show, setShow] = useState(false)
+function Authorization(){
+  return(
+    <View style={{display:"flex", flex:1, flexDirection:'column'}}>
+      <View style={{flex:11, marginLeft:10, padding: 0}}>
+        <Image source={require('./images/IT.png')} />
+      </View>
+      <View style={{flex:1}} >
+        <Image source={require('./images/Bilim-Logo.png')} />
+      </View>
+      <View style={{flex:2, display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
+        <Button title="Log In" color="#841584" onPress={() => setShow(true)}/>
+        <Button title="Register" color="#841584" onPress={() => setShow(true)} />
+      </View>
     </View>
-  );
+  )
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator activeColor="#f0f7f4">
+      <Tab.Screen name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }} />
+      <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarLabel: 'Favorites',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="star" color={color} size={26} />
+          ),
+        }}
+      />
+      
+      
+    </Tab.Navigator>
+  )
+}
+function check(){
+  if(!show){
+    return(
+      <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+    )
+  }else{
+    <Authorization />
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +73,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default function App(){
+  return (
+    check()
+  )
+};  
